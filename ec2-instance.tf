c9b157017b398e3cada259afba8878549cc68d21
Ricahrd E <rdeseke@swcp.com>
2024-10-27 14:56:20 +0100

    rebuild from remote head

--- ec2-instance.tf
+++ ec2-instance.tf
@@ -30,8 +30,6 @@ resource "aws_key_pair" "our_public_ssh_key" {
   public_key = var.aws_ssh_key  #defined in screts
 }
 
-
-
 # RESOURCE 2) an "aws_security_group" is like the rules what network connections are 
 #             allowed for the "aws_instance" we use this resource with
 resource "aws_security_group" "our_security_group" {
@@ -68,7 +66,7 @@ resource "aws_instance" "Alma8_community" {
   availability_zone = "eu-central-1"
 
   # ami           = var.base_ami
-  instance_type = var.instance_type
+  instance_type   = var.instance_type
   ami             = var.ami_my_image
 
   # clearly we want to be able to access it via ssh, hence our key is reverenced
@@ -97,17 +95,3 @@ output "instance_ip" {
   description = "The public ip for ssh access"
   value       = aws_instance.Alma8_community.public_ip
 }
-
-  # First thing to select it the "image" (amazon machine image)
-  # that we want to use with our instance. (via `aws ec2 decribe-images` you will 
-  # get a large JSON back with all images existing)
-  # the image we use is 
-  # "ami-07034695835d8f3bd" is arm64 amazon ubuntu 22.04 minimal
-  # because ubuntu is good for testing + arm64 is the architecture of "t4g.nano" instance_type
-  
-  # AMI is Alma8 plain region eu-center-1 (from Vars file)
-  # ami = var.ami_imag
-
-  # We select the type of instance we want
-  # t4g.nano is cheapest hourly rate at 0.0048 USD per hour or 3.45 USD per month
-  # on demand pricing 
