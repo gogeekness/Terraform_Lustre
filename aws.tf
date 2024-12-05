bb2cd654e980bb421f2c5503856b0bebe49a0f28
Ricahrd E <rdeseke@swcp.com>
2024-12-05 16:14:15 +0100

    The module slit is working

---
+++ aws.tf
@@ -0,0 +1,27 @@
+# terraform is now "commercial/proprietary" hence we go with drop replacement opentofu (free software)
+# Opentofu/Terraform work by writing "resources" that one wants to be created
+# those resources can then
+# a) be easily planned + applyed (i.e. created) 
+#    # tofu plan -out ourplan
+#    # tofu apply outplan
+# b) be as easily removed/deletet/destroyed 
+#    # tofu destroy
+#
+# in this file we setup the "modules" and "terraform" stuff 
+# so that aws resources can be managed via terraform 
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region  = var.region
+  access_key = var.Access_Key_ID
+  secret_key = var.Sercret_Access_Key
+}
