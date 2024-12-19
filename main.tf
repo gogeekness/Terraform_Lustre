b27c82f68104589ffa3e4772dc88686983a4ddf4
Ricahrd E <rdeseke@swcp.com>
2024-12-19 10:20:53 +0100

    adding Ansible to configure the cluster

--- main.tf
+++ main.tf
@@ -5,8 +5,8 @@
 module "lust_net" {
   source = "./Lustre_Net"
 
-  region             = var.region
-  availability_zone  = var.availability_zone
+  region            = var.region
+  availability_zone = var.availability_zone
 }
 
 # for each node of this small cluster
@@ -18,17 +18,17 @@ variable "server_list" {
   }))
   default = [
     {
-      host_name       = "lustre_mgt" 
-      instance_type   = "t3a.large"
-      ipv4            = "10.0.1.10"
+      host_name     = "lustre_mgt"
+      instance_type = "t3a.large"
+      ipv4          = "10.0.1.10"
     },
     {
-      host_name       = "lustre_oss"  
-      instance_type   = "t3.xlarge"
-      ipv4            = "10.0.1.11"
-    },    
+      host_name     = "lustre_oss"
+      instance_type = "t3.xlarge"
+      ipv4          = "10.0.1.11"
+    },
     {
-      host_name     = "lustre_client" 
+      host_name     = "lustre_client"
       instance_type = "t2.micro"
       ipv4          = "10.0.1.12"
     }
@@ -55,8 +55,8 @@ resource "aws_ebs_volume" "data_drives" {
   for_each = toset(local.server_names)
 
   availability_zone = module.lust_net.availability_zone
-  size             = 30  #GB
-  type             = "gp3"
+  size              = 30 #GB
+  type              = "gp3"
   tags = {
     Name = "data-drive-${each.key}"
   }
@@ -65,36 +65,55 @@ resource "aws_ebs_volume" "data_drives" {
 # EBS volumes main Data drive 500 GB drive for the oss
 resource "aws_ebs_volume" "zfs_data_drive" {
   availability_zone = module.lust_net.availability_zone
-  size             = 500  #GB 
-  type             = "gp3"
+  size              = 500 #GB 
+  type              = "gp3"
   tags = {
     Name = "data-drive-oss-server"
   }
 }
 
+# Need to attach the data drives 
+resource "aws_volume_attachment" "data_drive_attachments" {
+  for_each = { for server in var.server_list : server.host_name => server }
+
+  device_name = "/dev/sdd" # Set as second drive 
+  volume_id   = aws_ebs_volume.data_drives[each.key].id
+  instance_id = aws_instance.Lustre_servers[each.key].id
+}
+
+# Attach large drive specifically to OSS server
+resource "aws_volume_attachment" "oss_large_drive" {
+  device_name = "/dev/sdz" # Set as -last- drive for the oss
+  volume_id   = aws_ebs_volume.zfs_data_drive.id
+  instance_id = aws_instance.Lustre_servers["lustre_oss"].id
+}
+
+
+
+
 
 resource "aws_key_pair" "Lustre_Key" {
   # the name for the resource
-  key_name  = "Lustre_Key"
+  key_name   = "Lustre_Key"
   public_key = var.aws_key_pub
   # public_key = file("./ssh/lustretest.pub")  #defined in screts
 }
 
 
 resource "aws_instance" "Lustre_servers" {
-  for_each        = { for server in var.server_list : server.host_name => server }
+  for_each = { for server in var.server_list : server.host_name => server }
   #for_each        = toset(var.server_list)
 
   # host_id         = "${each.key}"
-  instance_type   = each.value.instance_type
-  ami             = var.ami_my_image
-  subnet_id       = module.lust_net.subnet_id
-  private_ip      = each.value.ipv4
-  key_name        = aws_key_pair.Lustre_Key.key_name
-  availability_zone = module.lust_net.availability_zone
+  instance_type               = each.value.instance_type
+  ami                         = var.ami_my_image
+  subnet_id                   = module.lust_net.subnet_id
+  private_ip                  = each.value.ipv4
+  key_name                    = aws_key_pair.Lustre_Key.key_name
+  availability_zone           = module.lust_net.availability_zone
   associate_public_ip_address = each.key == "lustre_client" ? true : false
 
-    # the one we created as "RESOURCE 1) Also we now use the "aws_security_group" of RESOURCE 2) above
+  # the one we created as "RESOURCE 1) Also we now use the "aws_security_group" of RESOURCE 2) above
   vpc_security_group_ids = [module.lust_net.security_group.id]
 
   tags = {
--- variables.tf
+++ variables.tf
@@ -8,55 +8,55 @@
 #   sensitive = true
 # }
 
-variable "aws_key_pub"{
-    type = string
-    description = "Main SSH Key"
-    # default = "Non-usable-keypair"
-    sensitive = true
-}
-
-variable "region" {
-    type = string
-    description = "Active region for deployments"
-    sensitive = false
-    # default = "eu-central-1"
-}
-
-variable "Access_Key_ID" {
-  type = string
-  description = "Access ID to gain access to AWS CLI"
-  sensitive = true 
-}
-
-variable "Sercret_Access_Key" {
-  type = string
-  description = "Access Secret to gain access to AWS CLI"
+variable "aws_key_pub" {
+  type        = string
+  description = "Main SSH Key"
+  # default = "Non-usable-keypair"
   sensitive = true
 }
 
+variable "region" {
+  type        = string
+  description = "Active region for deployments"
+  sensitive   = false
+  # default = "eu-central-1"
+}
+
+variable "Access_Key_ID" {
+  type        = string
+  description = "Access ID to gain access to AWS CLI"
+  sensitive   = true
+}
+
+variable "Sercret_Access_Key" {
+  type        = string
+  description = "Access Secret to gain access to AWS CLI"
+  sensitive   = true
+}
+
 variable "instance_type" {
-  type = string
+  type        = string
   description = "Instance Size"
-  sensitive = false
+  sensitive   = false
 }
 
 # marks the last snapshot of the Lustre install
 variable "lustre_snapshot" {
-  type = string
+  type        = string
   description = "The Lusture slice OS image"
-  default = "snap-05443946df91a3b0f"
-  sensitive = false
+  default     = "snap-05443946df91a3b0f"
+  sensitive   = false
 }
 #lustre_snapshot
 
 variable "ami_my_image" {
-  type = string
+  type        = string
   description = "The ami for the Lustre"
-  default = "ami-08bacc8479e361271"
-  sensitive = false
+  default     = "ami-08bacc8479e361271"
+  sensitive   = false
 }
 
 variable "availability_zone" {
-  type = string
+  type        = string
   description = "VPC zone"
 }
