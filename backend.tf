394a6b754a59d4dd6f443a999e0794897a6fbb73
Ricahrd E <rdeseke@swcp.com>
2025-03-10 12:47:08 +0100

    Update gitignore

--- backend.tf
+++ backend.tf
@@ -1,12 +1,12 @@
 # AWS backend to keep the state of the Lusre cluster
 # Opentofu/Terraform work by writing "resources" that one wants to be created
 
-# terraform {
-#   backend "s3" {
-#     bucket = "lustre-config"
-#     key = "./terraform.tfstate"
-#     region     = var.region
-#     access_key = var.Access_Key_ID
-#     secret_key = var.Sercret_Access_Key
-#   }
-# }
+terraform {
+  backend "s3" {
+    bucket = "lustre-config"
+    key = "./terraform.tfstate"
+    region     = var.region
+    access_key = var.Access_Key_ID
+    secret_key = var.Sercret_Access_Key
+  }
+}
